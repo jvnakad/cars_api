5e016c4617fa95c76a4a86a06e3633a004c4fdc3
jvnakad <112445763+jvnakad@users.noreply.github.com>
2025-11-20 21:43:10 +0000

    adjust length json

--- pages/api/track.js
+++ pages/api/track.js
@@ -1,44 +1,42 @@
 function track(request, response) {
-  // add cache
   response.setHeader("Cache-Control", "s-maxage=10, stale-while-revalidate");
-
   response.status(200).json({
     tracks: [
       {
         id: 1,
         name: "Radiator Springs Speedway",
         location: "Radiator Springs",
-        length: "2.5 miles",
+        lengthInMiles: 2.5,
         type: "asphalt oval",
         recordTime: "1:22.5",
-        recordHolderId: 1, // Lightning McQueen
+        recordHolderId: 1,
       },
       {
         id: 2,
         name: "Los Angeles Grand Prix",
         location: "Los Angeles",
-        length: "3.1 miles",
+        lengthInMiles: 3.1,
         type: "street circuit",
         recordTime: "1:45.3",
-        recordHolderId: 2, // Chick Hicks
+        recordHolderId: 2,
       },
       {
         id: 3,
         name: "Tokyo Drift Circuit",
         location: "Tokyo",
-        length: "2.8 miles",
+        lengthInMiles: 2.8,
         type: "technical track",
         recordTime: "1:36.9",
-        recordHolderId: 1, // Lightning McQueen
+        recordHolderId: 1,
       },
       {
         id: 4,
         name: "Florida International Speedway",
         location: "Florida",
-        length: "2.7 miles",
+        lengthInMiles: 2.7,
         type: "high-speed oval",
         recordTime: "1:20.2",
-        recordHolderId: 3, // Strip The King
+        recordHolderId: 3,
       },
     ],
   });
